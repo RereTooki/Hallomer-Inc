4a2a3ae44c31910ae01ae53f111a457ea05a572f
RereTooki <rereloluwa.afolabi.tooki@gmail.com>
2025-03-14 12:35:54 +0100

    Initial commit for 2nd Hallomer

---
+++ src/components/Homepage.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "../App.css";
+
+const Homepage = () => {
+  return (
+    <>
+      <h1 className="text-6xl font-bold font-tusker underline text-center">
+        {" "}
+        Hallomer
+      </h1>
+      {/* <p
+        className="text-6xl font-bold interFont underline text-center"
+        data-aos="zoom-in"
+        data-aos-duration="500"
+      >
+        ssss rere
+      </p> */}
+    </>
+  );
+};
+
+export default Homepage;

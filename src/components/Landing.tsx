0b9e867827b42675ad810a3b5a89d754774360fc
RereTooki <rereloluwa.afolabi.tooki@gmail.com>
2025-03-14 22:58:29 +0100

    done

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import flags from "../assets/images/FLAGS.png";
+import flagsL from "../assets/icons/flagsLarge.svg";
+import flagsS from "../assets/icons/flagsSmall.svg";
 import arrDown from "../assets/icons/arrDown.svg";
 
 const Landing = () => {
@@ -11,23 +13,18 @@ const Landing = () => {
   return (
     <>
       <div
-        className="text-white h-dvh flex flex-col items-center justify-center border-4 px-4 selection:bg-white selection:text-[#111]"
+        className="text-white h-dvh flex flex-col items-center justify-center sborder-4 px-4 selection:bg-white selection:text-[#111]"
         data-aoss="zoom-in"
         data-aoss-duration="1200"
       >
-        <div className="srelative bsorder-4 border-white  w-[88%] h-fit mx-12 select-none">
-          <div className=" sborder-4 transition ease-in-out duration-500 delay-10 cursor-pointer hover:scale-[1.04] mxm-12 kw-full h-fit sabsolute top-[-15px] xmd:top-[-25px] z-30">
-            <img
-              className="w-full h-full drop-shadow-[3px_6px_5px_rgba(0,0,0,0.25)] relative bottom-[-20px] z-40"
-              src={flags}
-              alt="flags"
-            />
-          </div>
+        <div className="sborder-4 transition ease-in-out duration-500 delay-10 cursor-pointer hover:scale-[1.04] w-[88%] h-fit mx-12 select-none w-fitd h-fit z-20">
+          <img className="w-fit h-fit" src={flagsL} alt="flags" />
         </div>
-        <h1 className="sborder-4 font-tusker text-[18vw] xmd:stext-[16vw] swhitespace-nowrap transition ease-in-out duration-500 delay-10 cursor-pointer shover:scale-[1.04] w-full text-center border-white/20 select-none relative bottom-24 z-10 ">
+
+        {/* <h1 className="sborder-4 font-tusker text-[18vw] xmd:stext-[16vw] swhitespace-nowrap transition ease-in-out duration-500 delay-10 cursor-pointer shover:scale-[1.04] w-full text-center border-white/20 select-none ">
           EDRISS HALLOMER
-        </h1>
-        <p className="interFont sborder-4 text-sm text-center relative bottom-20 ">
+        </h1> */}
+        <p className="interFont sborder-4 text-sm text-center relative bottom-4 ">
           A dynamic and multifaceted professional with over six years of
           experience spanning various roles including Event Project Management,
           Strategic Planning, Disk Jockeying, Sales Representative, Managing
